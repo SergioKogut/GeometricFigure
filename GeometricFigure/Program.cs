f193696848c5a4c85b41dde5dc5806a6532da451
Sergio <sergio.kogut@gmail.com>
2018-04-22 19:27:13 +0200

    end task

--- GeometricFigure/Program.cs
+++ GeometricFigure/Program.cs
@@ -6,10 +6,251 @@ using System.Threading.Tasks;
 
 namespace GeometricFigure
 {
+    interface IFigure
+    {
+        double GetPerimetr();
+        double GetArea();
+        string ToString();
+    }
+
+    class Triangle: IFigure     
+    {
+        private double a, b, c;
+
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+       public double GetArea()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Round(Math.Sqrt(p * (p - a) * (p - b) * (p - c)),2); 
+        }
+
+        public double  GetPerimetr()
+        {
+            return a+b+c;
+        }
+        
+        public override string ToString()
+        {
+            return $" Triangle: a={a}cm, b={b}cm, c={c}cm\n Perimetr triangle:{GetPerimetr()}cm\n Area triangle:{GetArea()}cm2\n";
+        }
+    }
+
+    class Square : IFigure
+    {
+        private double a;
+
+        public Square(double a)
+        {
+            this.a = a;
+        }
+
+        public double GetArea()
+        {
+            return a*a;
+        }
+
+        public double GetPerimetr()
+        {
+            return 4*a;
+        }
+
+        public override string ToString()
+        {
+            return $" Square: a={a}cm\n Perimetr square:{GetPerimetr()}cm\n Area square:{GetArea()}cm2\n";
+        }
+    }
+
+    class Rhomb : IFigure
+    {
+        private double a,h;
+
+        public Rhomb(double a, double h)
+        {
+            this.a = a;
+            this.h = h;
+        }
+
+        public double GetArea()
+        {
+            return a * h;
+        }
+
+        public double GetPerimetr()
+        {
+            return 4*a;
+        }
+
+        public override string ToString()
+        {
+            return $" Rhomb: a={a}cm, h={h}cm\n Perimetr rhomb:{GetPerimetr()}cm\n Area rhomb:{GetArea()}cm2\n";
+        }
+    }
+
+    class Rectangle : IFigure
+    {
+        private double a, b;
+
+        public Rectangle(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public double GetArea()
+        {
+            return a * b;
+        }
+
+        public double GetPerimetr()
+        {
+            return 2 * a + 2 * b;
+        }
+
+        public override string ToString()
+        {
+            return $" Rectangle: a={a}cm, b={b}cm\n Perimetr rectangle:{GetPerimetr()}cm\n Area rectangle:{GetArea()}cm2\n";
+        }
+    }
+
+    class Parallelogram : IFigure
+    {
+        private double a, b,h;
+
+        public Parallelogram(double a, double b, double h)
+        {
+            this.a = a;
+            this.b = b;
+            this.h = h;
+        }
+
+        public double GetArea()
+        {
+            return a * h;
+        }
+
+        public double GetPerimetr()
+        {
+            return 2 * a + 2 * b;
+        }
+
+        public override string ToString()
+        {
+            return $" Parallelogram: a={a}cm, b={b}cm, h={h}cm\n Perimetr parallelogram:{GetPerimetr()}cm\n Area parallelogram:{GetArea()}cm2\n";
+        }
+    }
+
+    class Trapeze : IFigure
+    {
+        private double a, b, h;
+
+        public Trapeze(double a, double b, double h)
+        {
+            this.a = a;
+            this.b = b;
+            this.h = h;
+        }
+
+        public double GetArea()
+        {
+            return Math.Round(0.5 *(a +b)*h,2);
+        }
+
+        public double GetPerimetr()
+        {
+            return 2 * a + 2 * b;
+        }
+
+        public override string ToString()
+        {
+            return $" Trapeze: a={a}cm, b={b}cm, h={h}cm\n Perimetr trapeze:{GetPerimetr()}cm\n Area trapeze:{GetArea()}cm2\n";
+        }
+    }
+    
+ class Сircle : IFigure
+    {
+        private double r;
+
+        public Сircle(double r)
+        {
+            this.r = r;
+            
+        }
+
+        public double GetArea()
+        {
+            return Math.Round(Math.PI*r*r,2);
+        }
+
+        public double GetPerimetr()
+        {
+            return Math.Round(2 * Math.PI * r,2);
+        }
+
+        public override string ToString()
+        {
+            return $" Сircle: radius r={r}cm\n Lenght circle:{GetPerimetr()}cm\n Area circle:{GetArea()}cm2\n";
+        }
+    }
+    
+
+        class Ellipse : IFigure
+    {
+        private double a,b;
+
+        public Ellipse(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+
+        }
+
+        public double GetArea()
+        {
+            return Math.Round(Math.PI * a * b,2);
+        }
+
+        public double GetPerimetr()
+        {
+            return Math.Round(2 * Math.PI*Math.Sqrt(0.5*(a*a+b*b)),2);
+        }
+
+        public override string ToString()
+        {
+            return $" Ellipse: small semiosphere ellipse a={a}cm, large semiosphere ellipse b={b}cm \n Lenght ellipse:{GetPerimetr()}cm\n Area ellipse:{GetArea()}cm2\n";
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            List<IFigure> Figures=new List<IFigure> { new Triangle(10, 15, 20),//трикутник
+                                                      new Square(5),//квадрат
+                                                      new Rectangle(5, 12),//прямокутник
+                                                      new Rhomb(10, 5),//ромб
+                                                      new Parallelogram(10, 5, 4),//паралелограм
+                                                      new Trapeze(10, 5, 4),//трапеція
+                                                      new Сircle(17),//круг
+                                                      new Ellipse(10,7)};//еліпс
+
+
+            Console.WriteLine(" FIGURES:\n");
+            foreach (var figure in Figures)
+            {
+                Console.WriteLine(figure);
+
+            }
+            
+
+    
         }
     }
 }
